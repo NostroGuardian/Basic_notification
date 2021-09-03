662127cf602033a7076a10b772514643372574ac
NostroGuardian <shmelivan5@yandex.ru>
2021-09-03 21:24:52 +0300

    issue fixed

--- app/src/main/java/com/nostrodev/myapplication/MainActivity.java
+++ app/src/main/java/com/nostrodev/myapplication/MainActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -14,25 +18,36 @@ public class MainActivity extends AppCompatActivity {
 
     private static final int NOTIFY_ID = 101;
     private static final String NOTIFY_CHANNEL = "myAppNotification";
+    private static final String CHANNEL_ID = "1";
+
+    Notification notification;
+    NotificationManagerCompat notificationManagerCompat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, NOTIFY_CHANNEL, NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("Test notification")
+                .setContentText("This is the test notification");
+
+        notification = builder.build();
+        notificationManagerCompat = NotificationManagerCompat.from(this);
+
         Button button = findViewById(R.id.mainButton);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this, NOTIFY_CHANNEL)
-                        .setSmallIcon(R.mipmap.ic_launcher)
-                        .setContentTitle("MyApp notification")
-                        .setContentText("This is test notification.")
-                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
-
-                NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(MainActivity.this);
-                notificationManagerCompat.notify(NOTIFY_ID, builder.build());
+                notificationManagerCompat.notify(NOTIFY_ID, notification);
 
             }
         });
